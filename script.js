08c890aaf01075d273c880fed25f67ad42d2e891
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-06-24 15:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ function firstChar(text) {
 		t=text.charAt(0);
 		return t;
 	}
+	if(text.charAt(0)==""){
+		t=" ";
+		return t;
+	}
+	
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
+
 alert(firstChar(text));
